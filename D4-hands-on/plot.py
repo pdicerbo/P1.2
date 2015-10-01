33dfc78d8d2b192d452d5cc7af9337919e0e0a47
pdicerbo <pdicerbo@cn02-04.(none)>
2015-10-01 17:26:33 +0200

    perform media of data set

--- D4-hands-on/plot.py
+++ D4-hands-on/plot.py
@@ -1,19 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# import sys.argv as ar
 
-# if len(ar) < 2:
-#     print "\n\tUsage: %s namefile\n" % ar[0]
-# else:
-#     print "\n\tOk\n"
 
-print "\nMaking plot\n"
-
-matrix = np.loadtxt("output.txt", dtype=float)
+matrix = np.loadtxt("output.txt")
 ncores = matrix[:,0]
 band   = matrix[:,1]
 
+NMes = 10; i = 0; j = 0; k = 0;
+
+n_c = np.zeros(6)
+mesures = np.zeros(6)
+
+while i < len(band):
+    tmp = 0.
+    for j in range(NMes):
+        tmp += band[j + i]
+
+    mesures[k] = tmp / NMes
+    n_c[k] = ncores[i]
+    i += NMes; k += 1
+
 plt.figure()
-plt.plot(ncores, band)
-# plt.show()
+plt.plot(n_c, mesures, '-o')
 plt.savefig("plot.png")
