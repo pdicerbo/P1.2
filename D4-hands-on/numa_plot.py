e6f6f423115ab330354d3be7e18867c6311c94bf
pdicerbo <pdicerbo@login1.(none)>
2015-10-01 17:52:47 +0200

    adding numa scripts

---
+++ D4-hands-on/numa_plot.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+matrix = np.loadtxt("numa00_output.txt")
+ncores = matrix[:,0]
+band   = matrix[:,1]
+
+NMes = 10; i = 0; j = 0; k = 0;
+
+n_c = np.zeros(6)
+mesures = np.zeros(6)
+
+while i < len(band):
+    tmp = 0.
+    for j in range(NMes):
+        tmp += band[j + i]
+
+    mesures[k] = tmp / NMes
+    n_c[k] = ncores[i]
+    i += NMes; k += 1
+
+matrix = np.loadtxt("numa01_output.txt")
+ncores = matrix[:,0]
+band   = matrix[:,1]
+
+NMes = 10; i = 0; j = 0; k = 0;
+
+mesures2 = np.zeros(6)
+
+while i < len(band):
+    tmp = 0.
+    for j in range(NMes):
+        tmp += band[j + i]
+
+    mesures2[k] = tmp / NMes
+    i += NMes; k += 1
+
+plt.figure()
+plt.plot(n_c, mesures, '-o')
+plt.plot(n_c, mesures2, '-o')
+plt.savefig("numa_plot.png")
