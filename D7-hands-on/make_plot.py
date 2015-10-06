16e2fbc424646d97952cc1f1a9d2826c84cff981
pdicerbo <pierluigi.dicerbo@gmail.com>
2015-10-06 17:02:50 +0200

    first commit

---
+++ D7-hands-on/make_plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt#; plt.rcdefaults()
+import numpy as np
+#import matplotlib.pyplot as plt
+
+atlas_gflop_s = 3.716e+02
+mkl_gflop_s = 4.745e+02
+
+mkl_walltime = 795.21
+atlas_walltime = 1015.55
+
+libs = ('mkl-gnu', 'atlas')
+y_pos = np.zeros(2)
+y_pos[0] = mkl_gflop_s; y_pos[1] = atlas_gflop_s
+x_pos = np.arange(len(y_pos))
+bar_width = 0.35
+
+plt.bar(x_pos, y_pos, bar_width, align='center')
+plt.xticks(x_pos, libs)
+plt.xlabel('library')
+plt.ylabel('GFLOPS/s')
+
+plt.show()
